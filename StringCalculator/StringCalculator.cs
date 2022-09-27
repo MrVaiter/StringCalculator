8a913aec466db95383f666ec79ee9780921df4e3
Dmytro Molchanov <dima-25@ukr.net>
2022-09-27 20:27:20 +0300

    Create Add method in lib

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -4,6 +4,22 @@ namespace StringCalculatorLibrary
 {
     public class StringCalculator
     {
-        
+        public int Add(string numbers)
+        {
+            int result = 0;
+
+            if (numbers == "")
+                return 0;
+
+            if (!numbers.Contains(","))
+                return int.Parse(numbers);
+
+            foreach (var number in numbers.Split(',', '\n'))
+            {
+                result += int.Parse(number);
+            }
+
+            return result;
+        }
     }
 }
